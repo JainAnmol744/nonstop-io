c7c4d49ca16b455579833614b9debd3f48f54bb7
Anmol Jain <anmol@emsec.uk>
2024-03-14 20:24:08 +0530

    remove unwanted dependency

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "./responsive.css";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="237632193869-u21ji3c174d0t73kbnvasc17qjt66gni.apps.googleusercontent.com">
     <App />
-    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
